15a9a822ac62d17ee322b1b382caaf327de76fb9
Anastassia <nastyalobacheva1@gmail.com>
2026-07-29 21:06:07 +0300

    remove try catch block

--- backend/src/modules/languages/languages.controller.ts
+++ backend/src/modules/languages/languages.controller.ts
@@ -1,13 +1,7 @@
-
-import { DatabaseError } from "../generic/errors";
 import type { Request, Response } from "express";
 import { getLanguages } from "./languages.service";
 
 export const languageController = async (_: Request, res: Response) => {
-    try {
-        const languages = await getLanguages();
-        return res.status(200).json(languages);
-    } catch (error: unknown) {
-        throw error;
-    }
+    const languages = await getLanguages();
+    return res.status(200).json(languages);
 };
